ba26c013786ae4a78733eda9893809dec1e31d24
minhld <minhld38@gmail.com>
2026-08-07 13:54:10 -0500

    fixed the profile to local

--- src/main/java/com/minh/lee2/controller/MainController.java
+++ src/main/java/com/minh/lee2/controller/MainController.java
@@ -12,7 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Profile("dev")
+@Profile("local")
 @RestController
 @RequestMapping(path = "/v1")
 public class MainController {
